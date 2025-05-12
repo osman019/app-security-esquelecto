eec7aac72f8eb0a2ca26b6a549aaab92ca9c6c59
osman019 <osmanortizrolon@gmail.com>
2025-05-12 17:00:24 -0500

    feat: :sparkles: update

---
+++ src/main/java/com/crudsec/app_security_app/application/services/IUserService.java
@@ -0,0 +1,5 @@
+package com.crudsec.app_security_app.application.services;
+
+public interface IUserService {
+
+}
---
+++ src/main/java/com/crudsec/app_security_app/domain/entity/Role.java
@@ -0,0 +1,5 @@
+package com.crudsec.app_security_app.domain.entity;
+
+public class Role {
+
+}
